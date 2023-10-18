0c0ec9e562c6894ed46b599f811b4168b7c4cfea
Nabeel Ahmed <nabeel.ahmed.ceative@gmail.com>
2023-10-18 19:53:56 +0500

    changes in ehb dashboard

--- src/components/authentication/signIn/signIn.tsx
+++ src/components/authentication/signIn/signIn.tsx
@@ -5,6 +5,7 @@ import authOverlay from '../../../assets/wrapper/overlay-auth.svg'
 import brandLogo from '../../../assets/brandAssets/brand-logo-fr-white.svg'
 import googleIcon from '../../../assets/icons/google.svg'
 import { useNavigate } from 'react-router-dom'
+import ehbIcon from '../../../assets/icons/ehb-companies/ehb-main-dark.svg'
 
 const SignIn = () => {
 
@@ -16,60 +17,58 @@ const SignIn = () => {
     const onFinishFailed = (errorInfo: any) => {
         console.log('Failed:', errorInfo);
     };
-    return (
-        <div className='auth-main-wrapper'>
-            <Row>
-                <Col lg={12} xl={12}>
-                    <div className="side-wrapper">
-                        <div className="inner-wrap">
-                            <img className="brand-logo" src={brandLogo} alt="" />
-                            <div className="content">
-                                <h3>Sign in and unlock a world of local services .</h3>
-                            </div>
-                            <img className='overlay-auth' src={authOverlay} alt="" />
-                        </div>
-                    </div>
-                </Col>
-                <Col lg={12} xl={12}>
-                    <div className="wrapper-auth">
-                        <h2 className='fs-32 fw-700' style={{ marginBottom: '30px' }}>Sign in</h2>
-                        <Form
-                            name="basic"
-                            initialValues={{ remember: true }}
-                            onFinish={onFinish}
-                            onFinishFailed={onFinishFailed}
-                            // autoComplete="off"
-                            layout="vertical">
-                            <Row>
-                                <Col xs={24} sm={24} md={24} lg={24}>
-                                    <Form.Item
-                                        label="Email"
-                                        name="email"
-                                        rules={[{ required: true, message: 'Required field' }]}
-                                    >
-                                        <Input placeholder="Type here" style={{ width: '100%', height: '45px', backgroundColor: "#FCFBFF" }} />
-                                    </Form.Item>
-                                </Col>
-                                <Col xs={24} sm={24} md={24} lg={24}>
-                                    <Form.Item
-                                        label="Password"
-                                        name="password"
-                                        rules={[{ required: true, message: 'Required field' }]}
-                                    >
-                                        <Input placeholder="Type here" style={{ width: '100%', height: '45px', backgroundColor: "#FCFBFF" }} />
-                                    </Form.Item>
-                                </Col>
 
-                                <div className="d-flex flex-column" style={{ gap: "15px", width: "100%", marginTop: "20px" }}>
-                                    <Button htmlType='submit' className='sign-in-button'>Sign In</Button>
-                                    <Button htmlType='button' className='sign-in-button-google'> <img src={googleIcon} width={15} height={15} alt="" /> Sign In with google</Button>
-                                    <p className='end-navigator text-center'>Doesn't have an account <span onClick={() => navigate('/sign-up')} >Sign up</span></p>
-                                </div>
-                            </Row>
-                        </Form>
+    return (
+        <div className='auth-main-wrapper tex-w'>
+            <div className='header-alg'>
+                <img src={ehbIcon} width={70} alt="" />
+                <p>Sign In to <strong>EHB</strong> </p>
+            </div>
+            <div className="wrapper-auth">
+                <Form
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    // autoComplete="off"
+                    layout="vertical">
+                    <Row gutter={[20, 0]}>
+                        <Col xs={24} sm={24} md={24} lg={24}>
+                            <Form.Item
+                                label="Email / Username"
+                                name="firstName"
+                                rules={[{ required: true, message: 'Required field' }]}
+                            >
+                                <Input placeholder="Type here" rootClassName='styled-input' autoComplete='off' />
+                            </Form.Item>
+                        </Col>
+                        <Col xs={24} sm={24} md={24} lg={24}>
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[
+                                    {
+                                      required: true,
+                                      message: 'Please enter your password!',
+                                    },
+                                  ]}
+                            >
+                                <Input.Password rootClassName='styled-input' placeholder="Enter your password" />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
+                        <Button htmlType='submit' className='common-btn-dull' style={{ width: "100%", marginTop: "30px" }}>Login</Button>
                     </div>
-                </Col>
-            </Row>
+                    <p className='bottom-res'>Didn't have an account ? <span onClick={() => navigate('/sign-up')}>Register</span></p>
+                </Form>
+            </div>
+            <div className='lb-docs-nav'>
+                    <p>Terms</p>
+                    <p>Privacy</p>
+                    <p>About</p>
+                    <p className='ex-w'><em>Contact EHB support</em></p>
+                </div>
         </div>
     )
 }
--- src/components/authentication/signUp/signUp.tsx
+++ src/components/authentication/signUp/signUp.tsx
@@ -5,7 +5,7 @@ import authOverlay from '../../../assets/wrapper/overlay-auth.svg'
 import brandLogo from '../../../assets/brandAssets/brand-logo-fr-white.svg'
 import googleIcon from '../../../assets/icons/google.svg'
 import faceVectorReg from '../../../assets/wrapper/face-vec-reg.png'
-import infoIcon from '../../../assets/icons/info-green.svg'
+import ehbIcon from '../../../assets/icons/ehb-companies/ehb-main-dark.svg'
 
 
 import { useNavigate } from 'react-router-dom'
@@ -17,10 +17,6 @@ const { Step } = Steps;
 const SignUp = () => {
 
     const navigate = useNavigate()
-    const [isSkipFacialVerification, setIsSkipFacialVerification] = useState(false)
-
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
 
     const onFinish = (values: any) => {
         console.log('Success:', values);
@@ -28,189 +24,163 @@ const SignUp = () => {
     const onFinishFailed = (errorInfo: any) => {
         console.log('Failed:', errorInfo);
     };
-    const handleCountryChange = (value: string) => {
-    };
 
+    const validateConfirmPassword = ({ getFieldValue }:any) => ({
+        validator(_:any, value:any) {
+            if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+            }
+            return Promise.reject(new Error('The two passwords do not match'));
+        },
 
+    });
+    const validateStrongPassword = (_:any, value:any) => {
+        // Use a regular expression to check for a strong password (minimum 8 characters, at least one uppercase letter, one lowercase letter, one number, and one special character)
+        const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    
+        if (!value || strongPasswordRegex.test(value)) {
+          return Promise.resolve();
+        }
+    
+        return Promise.reject(new Error('Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.'));
+      };
 
-    const [currentStep, setCurrentStep] = useState(0);
-
-    const handleNext = () => {
-        setCurrentStep(currentStep + 1);
-    };
-
-    const handlePrev = () => {
-        setCurrentStep(currentStep - 1);
-    };
-
-    console.log("currentStep", currentStep)
-
-
+    const phoneRegex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
     return (
-        <div className='auth-main-wrapper'>
-            <Row>
-                <Col lg={12} xl={12}>
-                    <div className="side-wrapper">
-                        <div className="inner-wrap">
-                            <img className="brand-logo" src={brandLogo} alt="" />
-                            <div className="content">
-                                <h3>Join Quiklers and discover a new way to access services .</h3>
-                            </div>
-                            <img className='overlay-auth' src={authOverlay} alt="" />
-                        </div>
+        <div className='auth-main-wrapper tex-w'>
+            <div className='header-alg'>
+                <img src={ehbIcon} width={70} alt="" />
+                <p>Register to <strong>EHB</strong> </p>
+            </div>
+            <div className="wrapper-auth">
+                <Form
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    // autoComplete="off"
+                    layout="vertical">
+                    <Row gutter={[20, 0]}>
+                        <Col xs={24} sm={24} md={24} lg={12}>
+                            <Form.Item
+                                label="First Name"
+                                name="firstName"
+                                rules={[{ required: true, message: 'Required field' }]}
+                            >
+                                <Input placeholder="Type here" rootClassName='styled-input' autoComplete='off' />
+                            </Form.Item>
+                        </Col>
+                        <Col xs={24} sm={24} md={24} lg={12}>
+                            <Form.Item
+                                label="Last Name"
+                                name="lastName"
+                                rules={[{ required: true, message: 'Required field' }]}
+                            >
+                                <Input placeholder="Type here" rootClassName='styled-input' />
+                            </Form.Item>
+                        </Col>
+                        <Col xs={24} sm={24} md={24} lg={24}>
+                            <Form.Item
+                                label="Email"
+                                name="email"
+                                rules={[
+                                    {
+                                        type: 'email',
+                                        message: 'Please enter a valid email address!',
+                                    },
+                                    {
+                                        required: true,
+                                        message: 'Email is required!',
+                                    },
+                                ]}
+                            >
+                                <Input placeholder="Type here" rootClassName='styled-input' autoComplete='off' />
+                            </Form.Item>
+                        </Col>
+                        <Col xs={24} sm={24} md={24} lg={24}>
+                            <Form.Item
+                                label="Select Country"
+                                name="country"
+                                rules={[{ required: true, message: 'Required field' }]}
+                            >
+                                <Select
+                                    showSearch
+                                    placeholder="Select a country"
+                                    rootClassName='selector-c'
+                                >
+                                    {countryOptions.map((option) => (
+                                        <Option key={option.value} value={option.value}>
+                                            {option.label}
+                                        </Option>
+                                    ))}
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col xs={24} sm={24} md={24} lg={24}>
+                            <Form.Item
+                                label="Phone"
+                                name="phone"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please enter your phone number!',
+                                    },
+                                    {
+                                        pattern: phoneRegex,
+                                        message: 'Invalid phone number format! (e.g., +123-4567890123 or 1234567890)',
+                                    },
+                                ]}
+                            >
+                                <Input placeholder="Type here" rootClassName='styled-input' autoComplete='off' />
+                            </Form.Item>
+                        </Col>
+                        <Col xs={24} sm={24} md={24} lg={24}>
+                            <Form.Item
+                                name="password"
+                                label="Password"
+                                rules={[
+                                    {
+                                      required: true,
+                                      message: 'Please enter your password!',
+                                    },
+                                    {
+                                      validator: validateStrongPassword,
+                                    },
+                                  ]}
+                            >
+                                <Input.Password rootClassName='styled-input' placeholder="Enter your password" />
+                            </Form.Item>
+                        </Col>
+                        <Col xs={24} sm={24} md={24} lg={24}>
+                            <Form.Item
+                                name="confirmPassword"
+                                label="Confirm Password"
+                                dependencies={['password']}
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please confirm your password!',
+                                    },
+                                    ({ getFieldValue }) => validateConfirmPassword({ getFieldValue }),
+                                ]}
+                            >
+                                <Input.Password rootClassName='styled-input' placeholder="Confirm your password" />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
+                        <Button htmlType='submit' className='common-btn-dull' style={{ width: "100%", marginTop: "30px" }}>Register</Button>
                     </div>
-                </Col>
-                <Col lg={12} xl={12}>
-                    <div className="wrapper-auth">
-                        <h2 className='fs-32 fw-700' style={{ marginBottom: '30px' }}>Sign Up</h2>
+                    <p className='bottom-res'>Already have an account ? <span onClick={() => navigate('/sign-in')}>Sign in</span></p>
+                </Form>
 
-                        <Steps current={currentStep} style={{ marginBottom: "40px" }}>
-                            <Step title="Step 1" description="Personal Information" />
-                            <Step title="Step 2" description="Verification" />
-                            <Step title="Step 3" description="Finalization" />
-                        </Steps>
-
-                        <Form
-                            name="basic"
-                            initialValues={{ remember: true }}
-                            onFinish={onFinish}
-                            onFinishFailed={onFinishFailed}
-                            // autoComplete="off"
-                            layout="vertical">
-                            <Row gutter={[20, 0]}>
-                                {
-                                    currentStep === 0 &&
-                                    <>
-                                        <Col xs={24} sm={24} md={24} lg={12}>
-                                            <Form.Item
-                                                label="First Name"
-                                                name="firstName"
-                                                rules={[{ required: true, message: 'Required field' }]}
-                                            >
-                                                <Input placeholder="Type here" style={{ width: '100%', height: '45px', backgroundColor: "#FCFBFF" }} />
-                                            </Form.Item>
-                                        </Col>
-                                        <Col xs={24} sm={24} md={24} lg={12}>
-                                            <Form.Item
-                                                label="Last Name"
-                                                name="lastName"
-                                                rules={[{ required: true, message: 'Required field' }]}
-                                            >
-                                                <Input placeholder="Type here" style={{ width: '100%', height: '45px', backgroundColor: "#FCFBFF" }} />
-                                            </Form.Item>
-                                        </Col>
-                                        <Col xs={24} sm={24} md={24} lg={24}>
-                                            <Form.Item
-                                                label="Email"
-                                                name="email"
-                                                rules={[{ required: true, message: 'Required field' }]}
-                                            >
-                                                <Input placeholder="Type here" style={{ width: '100%', height: '45px', backgroundColor: "#FCFBFF" }} />
-                                            </Form.Item>
-                                        </Col>
-                                        <Col xs={24} sm={24} md={24} lg={24}>
-                                            <Form.Item
-                                                label="Select Country"
-                                                name="country"
-                                                rules={[{ required: true, message: 'Required field' }]}
-                                            >
-                                                <Select
-                                                    showSearch
-                                                    placeholder="Select a country"
-                                                >
-                                                    {countryOptions.map((option) => (
-                                                        <Option key={option.value} value={option.value}>
-                                                            {option.label}
-                                                        </Option>
-                                                    ))}
-                                                </Select>
-                                            </Form.Item>
-                                        </Col>
-                                    </>
-                                }
-
-                                {
-                                    currentStep === 1 &&
-                                    <>
-                                        <div className='face-reg-wrapper'>
-                                            <h2>Face Verification</h2>
-                                            <p style={{ marginBottom: '20px' }}>Click "Start Face Verification" to do a face verification</p>
-                                            <div className="d-flex align-center flex-wrapper">
-                                                <img src={faceVectorReg} width={200} alt="" />
-                                                <Button htmlType='button' className='face-ver-button'>Start Face Verification</Button>
-                                            </div>
-
-                                            <Checkbox onClick={() => setIsModalOpen(true)} style={{ marginTop: '20px' }} checked={isSkipFacialVerification}>Skip this for now</Checkbox>
-
-                                        </div>
-                                    </>
-                                }
-
-                                {
-                                    currentStep === 2 &&
-                                    <>
-                                        <div className="passwords-wrapper">
-                                            <div style={{ width: '96.5%', margin: '0 auto', marginBottom: '25px' }}>
-                                                <h2>Great!</h2>
-                                                <p>Now setup your password</p>
-                                            </div>
-
-                                            <Col xs={24} sm={24} md={24} lg={24}>
-                                                <Form.Item
-                                                    label="Create Password"
-                                                    name="cPassword"
-                                                    rules={[{ required: true, message: 'Required field' }]}
-                                                >
-                                                    <Input placeholder="Type here" style={{ width: '100%', height: '45px', backgroundColor: "#FCFBFF" }} />
-                                                </Form.Item>
-                                            </Col>
-                                            <Col xs={24} sm={24} md={24} lg={24}>
-                                                <Form.Item
-                                                    label="Confirm Password"
-                                                    name="password"
-                                                    rules={[{ required: true, message: 'Required field' }]}
-                                                >
-                                                    <Input placeholder="Type here" style={{ width: '100%', height: '45px', backgroundColor: "#FCFBFF" }} />
-                                                </Form.Item>
-                                            </Col>
-
-                                        </div>
-                                    </>
-                                }
-
-
-                                <div className="d-flex flex-column" style={{ gap: "15px", width: "100%", marginTop: "20px" }}>
-                                    <div className="d-flex" style={{ gap: '10px' }}>
-                                        {currentStep > 0 && (
-                                            <Button htmlType='button' className='sign-in-button-ex button-dull' onClick={handlePrev}>Previous</Button>
-                                        )}
-                                        {currentStep < 2 && (
-                                            <Button htmlType='button' className='sign-in-button-ex' onClick={handleNext}>Next</Button>
-                                        )}
-                                        {currentStep === 2 && <Button htmlType='submit' className='sign-in-button'>Finish & Sign Up</Button>}
-                                    </div>
-
-
-                                    <p className='end-navigator'>Already have an account <span onClick={() => navigate('/sign-in')}>Sign in</span></p>
-                                </div>
-
-
-                                <Modal centered title="" open={isModalOpen} okText='Skip this for now' onOk={() => { setIsModalOpen(false); setIsSkipFacialVerification(true) }} onCancel={() => { setIsModalOpen(false); setIsSkipFacialVerification(false) }}>
-                                    <div className="d-flex flex-column align-center">
-                                    <img src={infoIcon} width={40} height={40} alt="" />
-                                    <h2 style={{marginTop:"20px"}}>Are you sure ?</h2>
-                                    <p style={{marginBottom:"20px", width:'80%', textAlign:'center'}}>By skipping face verification you were unable to sell or buy conditional services</p>
-                                    </div>
-                                   
-                                </Modal>
-
-
-                            </Row>
-                        </Form>
-
-                    </div>
-                </Col>
-            </Row>
+            </div>
+            <div className='lb-docs-nav'>
+                    <p>Terms</p>
+                    <p>Privacy</p>
+                    <p>About</p>
+                    <p className='ex-w'>Contact EHB support</p>
+                </div>
         </div>
     )
 }
