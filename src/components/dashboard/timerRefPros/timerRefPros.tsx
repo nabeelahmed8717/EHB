840644a5decaee9758657b4abd1b63c3450b677d
nabeel ahmed <nabeel.ahmed.ceative@gmail.com>
2023-11-13 18:40:28 +0500

    #56765 final push

--- src/components/dashboard/timerRefPros/timerRefPros.tsx
+++ src/components/dashboard/timerRefPros/timerRefPros.tsx
@@ -44,7 +44,6 @@ const TimerComponent = ({ startProp }: any) => {
     const remainingSeconds = seconds % 60;
 
     return `D:${String(days).padStart(2, '0')} H:${String(hours).padStart(2, '0')} M:${String(minutes).padStart(2, '0')} S:${String(remainingSeconds).padStart(2, '0')}`;
-
   };
 
   return (
